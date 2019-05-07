0687a8cb5672c433627080107e8802c384fa8394
hhhuy <hhuuyy95@gmail.com>
2019-05-07 17:41:53 +0700

    pagination and search customer

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,17 @@ Route::group(['prefix' => 'cities'], function () {
 });
 Route::group(['prefix' => 'customers'], function () {
     Route::get('/', 'CustomerController@index')->name('customers.index');
+
     Route::get('/create', 'CustomerController@create')->name('customers.create');
     Route::post('/create', 'CustomerController@store')->name('customers.store');
+
     Route::get('/{id}/edit', 'CustomerController@edit')->name('customers.edit');
     Route::post('/{id}/edit', 'CustomerController@update')->name('customers.update');
+
     Route::get('/{id}/destroy', 'CustomerController@destroy')->name('customers.destroy');
+
     Route::get('/filter', 'CustomerController@filterByCity')->name('customers.filterByCity');
+    Route::get('/search','CustomerController@search')->name('customers.search');
+
 });
 
